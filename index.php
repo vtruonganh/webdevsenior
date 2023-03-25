040e8e0ff9d12c81dbab9e0d664c8ba707493f6d
Vũ Trường Anh <92847275+vtruonganh@users.noreply.github.com>
2023-03-25 23:38:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="js.js">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="js.js" defer></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require "header.html";
+    ?>
+
+    <div id="wrapper-content">
+
+        <div class="banner">
+            <img src="https://devsenior.edu.vn/wp-content/uploads/2021/12/banner-home9.png" alt="">
+
+        </div>
+
+        <div class="title1">
+            <h1><span>DEVSENIOR AI </span>- A SOFTWARE <em>PRODUCTS COMPANY</em></h1>
+
+            <p>Specializing in artificial intelligence applications.</p>
+            <div class="buttonn" style="text-align: center;">
+                <a href="#"><button class="btn5-hover btn5">Let'start</button></a>
+                <div class="link">
+                    <a href="">Learn more</a>
+                </div>
+            </div>
+
+
+
+        </div>
+        <div class="section-gr">
+            <h1>THE POWER OF CREATIVITY</h1>
+            <div class="title">
+                <p>We develop a large number of small, fast products for short jobs, such as automatic facial makeup
+                    without
+                    wasting time. , automatically compose interviews, automatically find cheap airline tickets,
+                    automatically find saleoff orders, automatically find customers who need to rent a house,
+                    automatically
+                    find customers to sell houses, automatically look up, automatically find and sell houses.
+                    customer
+                    inquiries. Our company has 200 employees in 3 different locations, the headquarters in Louis
+                    city is
+                    the
+                    place where training and human resource development is combined, the sales department is located
+                    at
+                    69
+                    Vạn Phuc. Congratulations for your interest in our company and for taking the time to read this
+                    article.
+                    Thank you</p>
+                <a href="#">View more partners</a>
+            </div>
+
+        </div>
+
+
+        <div class="section-gr2">
+            <h1>We Provide <em>Different Services</em> & <span>Features</span> For Your Agency <em>The Power
+                    Of Creativity</em> </h1>
+            <div class="container">
+
+                <div class="grid">
+
+                    <div class="grid1">
+                        <div class="icon1">
+                            <img src="https://thinkdiff.us/assets/images/services-01.jpg" alt="">
+                        </div>
+                        <div class="title">
+                            <p>Discover more about the latest world trends</p>
+                        </div>
+                    </div>
+                    <div class="grid1">
+                        <div class="icon2">
+                            <img src="https://thinkdiff.us/assets/images/services-02.jpg" alt="">
+                        </div>
+                        <div class="title">
+                            <p>Analyze large requirements completely by customer</p>
+                        </div>
+                    </div>
+                    <div class="grid1">
+                        <div class="icon3">
+                            <img src="https://thinkdiff.us/assets/images/services-03.jpg" alt="">
+                        </div>
+                        <div class="title">
+                            <p>Precise Data Analysis & Prediction</p>
+                        </div>
+                    </div>
+                    <div class="grid1">
+                        <div class="icon4">
+                            <img src="https://thinkdiff.us/assets/images/services-04.jpg" alt="">
+                        </div>
+                        <div class="title">
+                            <p>35000+ Client</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <h1>Discover Our <em>Workplace</em> & <span>Member</span> </h1>
+        <div class="slideshow">
+
+            <i id="left" class="fa-solid fa-angle-left"></i>
+            <div class="carousel">
+                <img src="https://thinkdiff.us/assets/images/img01.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img036.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img034.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img037.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img033.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img031.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img032.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img010.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img09.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img038.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img031.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img032.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img039.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img040.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img041.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img042.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img043.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img049.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img050.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img051.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img052.jpg" alt="img" draggable="false">
+                <img src="https://thinkdiff.us/assets/images/img053.jpg" alt="img" draggable="false">
+                <!-- <img src="" alt="img" draggable="false">
+                <img src="" alt="img" draggable="false">
+
+                <img src="" alt="img" draggable="false">v
+
+                <img src="" alt="img" draggable="false"> ..-->
+            </div>
+            <i id="right" class="fa-solid fa-angle-right"></i>
+        </div>
+        <div class="section-gr3">
+            <div class="choose1">
+                <h3>Small Business Owner?</h3>
+                <img src="https://devsenior.edu.vn/wp-content/uploads/2021/12/box1-home9.png" alt="">
+                <p>We have a big number of solutions ready!<br>Starting at $199 - affordable.</p>
+                <a href="#">View More Details</a>
+            </div>
+            <div class="choose1">
+                <h3>Perhaps a Company?</h3>
+                <img src="https://devsenior.edu.vn/wp-content/uploads/2021/12/box2-home9.png" alt="">
+                <p>We have a big number of solutions ready! <br>Starting at $199 - affordable.</p>
+                <a href="#">View More Details</a>
+            </div>
+        </div>
+
+        <?php
+        require "footer.php";
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
